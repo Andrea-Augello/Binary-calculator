4f89284bba3d1a8e974bc2bb7b0c02a6bee671e5
Andrea-Augello <andrea.augello01@community.unipa.it>
2020-02-01 13:12:47 +0100

    Written output module

---
+++ src/output.f
@@ -0,0 +1,31 @@
+8 CONSTANT DISPLAY_SIZE
+CREATE DISPLAY 21 , 20 , 16 , 12 , 7 , 8 , 25 , 24 ,
+23 CONSTANT OVERFLOW
+18 CONSTANT NEGATIVE
+
+: LSB_MASK 				( shift n -- n/2 mask )
+	2 /MOD SWAP	ROT	( shift n -- n/2 LSB shift )
+	DISPLAY SWAP GET  ( n/2 LSB shift -- n/2 LSB display[shift] )
+	LSHIFT ;
+	
+: DISPLAY_MASK ( n -- mask ) 
+	0 0 			( n -- n mask loop_counter )
+	BEGIN
+		DUP 1 + >R
+		ROT LSB_MASK	( n mask loop_counter -- mask n/2 n_mask )
+		ROT AND 			\ adds the newly computed mask to the previous result
+		R> DUP
+		DISPLAY_SIZE <=
+	UNTIL
+	DROP SWAP ROT ;			
+
+: CLEAR ( -- )
+	[ 1 DISPLAY_SIZE LSHIFT 1 - DISPLAY_MASK ] IMMEDIATE 	\ mask with every display
+																			\ bit set to 1
+	GPCLR0 ! 
+	OVERFLOW OFF
+	NEGATIVE OFF ;
+: SHOW ( status n -- )  ;
+	CLEAR  DISPLAY_MASK GPSET0 ! 
+	DUP 1 AND OVERFLOW LSHIFT GPSET0 !
+	2 AND NEGATIVE LSHIFT GPSET0 ! ;
