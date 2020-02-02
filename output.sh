2f21ea6d6f96a36162b018733918d1703c8af46f
Andrea-Augello <andrea.augello01@community.unipa.it>
2020-02-02 14:14:33 +0100

    .

--- output.sh
+++ output.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd src
-cat se-ans.f setup.f logic.f output.f input.f > ../merged_src.f
+cat se-ans.f setup.f logic.f output.f input.f control.f > ../merged_src.f
 cd .. 
-./strip-comments.sh merged_src.f && mv stripped_merged_src.f merged_src.f
+./strip-comments.sh merged_src.f
+mv  stripped_merged_src.f merged_src.f
