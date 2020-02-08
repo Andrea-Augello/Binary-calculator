1295f88ebb6d4873dff42d6930a39d23cdd8c3bd
Andrea-Augello <andrea.augello01@community.unipa.it>
2020-02-08 18:33:12 +0100

    Renamed file

--- merge_source.sh
+++ merge_source.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd src
-cat se-ans.f setup.f logic.f output.f input.f control.f | 
+cat se-ans.f utils.f logic.f output.f input.f control.f | 
 awk -F"\\" '{print $1}' |  				# Removes '\' comments
 awk -F"[^A-Z]+[()][^A-Z]+" '{print $1 $    3}' | 	# Removes '( )' comments
 awk '{ printf "%s ", $0 }' |				# Removes newlines
