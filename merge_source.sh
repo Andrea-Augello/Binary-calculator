6cab36a34afb6a0013dea790f941ad333d1a384b
Andrea-Augello <andrea.augello01@community.unipa.it>
2020-02-08 17:54:17 +0100

    Removed unnecessary newlines

--- merge_source.sh
+++ merge_source.sh
@@ -2,6 +2,8 @@
 
 cd src
 cat se-ans.f setup.f logic.f output.f input.f control.f | 
-awk -F"\\" '{print $1}' | 
-awk -F"[^A-Z]+[()][^A-Z]+" '{print $1 $    3}' | 
-sed '/^[[:space:]]*$/d' > ../merged_src.f	
+awk -F"\\" '{print $1}' |  				# Removes '\' comments
+awk -F"[^A-Z]+[()][^A-Z]+" '{print $1 $    3}' | 	# Removes '( )' comments
+awk '{ printf "%s ", $0 }' |				# Removes newlines
+sed 's/\;/\;\n/g' |					# Adds a newline after ;
+sed '/^[[:space:]]*$/d' > ../merged_src.f   
